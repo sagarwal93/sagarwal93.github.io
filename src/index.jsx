20b3ee88577ad4963f9a6f1a2fd0ca03c73adc12
Samarth <samarth.agarwal1993@gmail.com>
2016-04-16 19:08:21 -0400

    Hash history trial

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute, useRouterHistory} from 'react-router';
+import {createHashHistory} from 'history';
 import Main from './components/Main';
 import About from './components/About';
 import Resume from './components/Resume';
 
 import './styles.css';
 
-const routes = <Router history={browserHistory}>
+const appHistory = useRouterHistory(createHashHistory)({queryKey: false});
+
+const routes = <Router>
   <Route name='main' path='/' component={Main}>
     <IndexRoute component={About} />
     <Route name='about' path='about' component={About} />
